16681562da1563bbb7249b7384dbcecbb2d75d5a
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-01-18 12:23:45 +0100

    shortened day of week

--- lua/plugins/git_integration.lua
+++ lua/plugins/git_integration.lua
@@ -18,7 +18,7 @@ return {
     "f-person/git-blame.nvim",
     event = "VeryLazy",
     config = function()
-      vim.g.gitblame_date_format = "%Y-%m-%d %A %H:%M:%S"
+      vim.g.gitblame_date_format = "%Y-%m-%d %a %H:%M:%S"
     end,
   },
 
