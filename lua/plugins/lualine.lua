383855fd78b76054f516fb9f00db4a7135af4d34
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-01-19 22:01:19 +0100

    added ctags generation notification

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -21,7 +21,7 @@ return {
           lualine_a = { "mode" },
           lualine_b = { "branch", "diff", "diagnostics" },
           lualine_c = { { "filename", path = 3 }, },
-          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_x = { "%{gutentags#statusline()}", "encoding", "fileformat", "filetype" },
           lualine_y = { "progress", "selectioncount" },
           lualine_z = { "location", "searchcount" },
         },
