97f6fbf13eaf79d7251814d713c8321c4c890e56
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-01-21 23:17:19 +0100

    autogroup for git blame line highlight

---
+++ lua/highlight_git_blame.lua
@@ -0,0 +1,31 @@
+function CreateHighlightGitBlameInline(event)
+  local highlight_comment = vim.api.nvim_get_hl(0, { name = "Comment" })
+  local highlight_git_blame = vim.tbl_deep_extend("keep", highlight_comment, {})
+  -- vim.print({ event = event, cursorline = vim.o.cursorline })
+  if vim.o.cursorline == true then
+    local highlight_cursorline = vim.api.nvim_get_hl(0, { name = "Cursorline" })
+    local cursorline_bg = highlight_cursorline.bg
+    if cursorline_bg then
+      highlight_git_blame.bg = cursorline_bg
+    end
+  end
+  vim.api.nvim_set_hl(0, "GitBlameInline", highlight_git_blame)
+end
+
+local highlight_group = vim.api.nvim_create_augroup("GitBlameInlineHighlight", { clear = true })
+
+for event_name, pattern in pairs({
+  ColorScheme = "*",
+  OptionSet = "cursorline",
+  -- VimEnter = "*",
+})
+do
+  vim.api.nvim_create_autocmd(
+    event_name,
+    {
+      callback = CreateHighlightGitBlameInline,
+      group = highlight_group,
+      pattern = pattern,
+      desc = "event_name = " .. event_name .. ", pattern = " .. pattern,
+    })
+end
