6b01c85040b3c71ae858acd01442d8719c41869f
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-01-21 18:30:27 +0100

    fixed error in mason-nvim-dap configuration

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -40,15 +40,15 @@ return {
       require("mason-nvim-dap").setup({
         -- Makes a best effort to setup the various debuggers with
         -- reasonable debug configurations
-        automatic_setup = true,
-
-        -- You can provide additional configuration to the handlers,
-        -- see mason-nvim-dap README for more information
-        handlers = {},
+        automatic_installation = true,
 
         -- You"ll need to check that you have the required things installed
         -- online, please don"t ask me how to install them :)
         ensure_installed = ensure_installed,
+
+        -- You can provide additional configuration to the handlers,
+        -- see mason-nvim-dap README for more information
+        handlers = {},
       })
 
       require("debugging_bash_debug_adapter").configure_bashdb(dap)
