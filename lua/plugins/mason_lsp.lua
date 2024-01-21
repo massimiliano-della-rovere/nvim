46350a446f4591ca83865ee4a19d07e01037751d
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-01-21 17:40:55 +0100

    added neodev as dependency

--- lua/plugins/mason_lsp.lua
+++ lua/plugins/mason_lsp.lua
@@ -30,11 +30,9 @@ return {
       },
 
       -- Neovim setup for init.lua and plugin development with full signature help, docs and completion for the nvim lua API.
-      {
-        -- https://github.com/folke/neodev.nvim
-        "folke/neodev.nvim",
-        opts = {}, -- NOTE: `opts = {}` is the same as calling `require('neodev').setup({})`
-      },
+      -- https://github.com/folke/neodev.nvim
+      -- already in programming.lua
+      "folke/neodev.nvim",
 
     },
     config = function()
