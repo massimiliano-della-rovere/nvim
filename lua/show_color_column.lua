ff2137ea370ffc08357cfa0b3af92f1dc5bba3fa
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-01-21 23:46:44 +0100

    added autocmd to autogroup

--- lua/show_color_column.lua
+++ lua/show_color_column.lua
@@ -1,7 +1,9 @@
 --  draw a vertical line to visualize textwidth
+local show_color_column_group = vim.api.nvim_create_augroup("ShowColorColumn", { clear = true })
 vim.api.nvim_create_autocmd(
   { "BufNewFile", "BufRead" },
   {
+    group = show_color_column_group,
     pattern = { "*.py", "*.sh", "*.bash" },
     callback = function()
       vim.opt_local.colorcolumn = { 80 }
