2c356e5703c66e05079d496373893bac32aa687e
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-01-21 18:35:12 +0100

    refactored installation

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -32,25 +32,29 @@ return {
 
       -- Installs the debug adapters for you
       "williamboman/mason.nvim",
-      "jay-babu/mason-nvim-dap.nvim",
+
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        config = function()
+          require("mason-nvim-dap").setup({
+            -- Makes a best effort to setup the various debuggers with
+            -- reasonable debug configurations
+            automatic_installation = true,
+
+            -- You"ll need to check that you have the required things installed
+            -- online, please don"t ask me how to install them :)
+            ensure_installed = ensure_installed,
+
+            -- You can provide additional configuration to the handlers,
+            -- see mason-nvim-dap README for more information
+            handlers = {},
+          })
+        end,
+      },
+
     },
     config = function()
       local dap = require("dap")
-
-      require("mason-nvim-dap").setup({
-        -- Makes a best effort to setup the various debuggers with
-        -- reasonable debug configurations
-        automatic_installation = true,
-
-        -- You"ll need to check that you have the required things installed
-        -- online, please don"t ask me how to install them :)
-        ensure_installed = ensure_installed,
-
-        -- You can provide additional configuration to the handlers,
-        -- see mason-nvim-dap README for more information
-        handlers = {},
-      })
-
       require("debugging_bash_debug_adapter").configure_bashdb(dap)
       require("debugging_debugpy").configure_debugpy(dap)
 
