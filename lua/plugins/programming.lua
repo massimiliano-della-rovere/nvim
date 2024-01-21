f9154a75a43311d074be57df05368d244c57d7f3
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-01-21 17:40:09 +0100

    removed duplicated neodev plugin

--- lua/plugins/programming.lua
+++ lua/plugins/programming.lua
@@ -157,6 +157,22 @@ return {
         "<CMD>AerialNavToggle<CR>",
         { desc = "Aerial: Toggle Nav" })
     end,
-  }
+  },
+
+  -- Neovim setup for init.lua and plugin development with full signature help, docs and completion for the nvim lua API.
+  {
+    -- https://github.com/folke/neodev.nvim
+    "folke/neodev.nvim",
+    dependencies = {
+      -- Creates a beautiful debugger UI
+      -- already in debugging.lua
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      require("neodev").setup({
+        library = { plugins = { "nvim-dap-ui" }, types = true },
+      })
+    end,
+  },
 
 }
