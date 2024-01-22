33c445697061e4e6e0617770b90b2aba268edeaa
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-01-22 10:41:19 +0100

    Added code inject plugin

--- lua/plugins/programming.lua
+++ lua/plugins/programming.lua
@@ -175,4 +175,19 @@ return {
     end,
   },
 
+  -- language injections
+  {
+    -- https://github.com/Dronakurl/injectme.nvim
+    "Dronakurl/injectme.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    lazy = true,
+    -- This is for lazy load and more performance on startup only
+    cmd = { "InjectmeToggle", "InjectmeSave", "InjectmeInfo" , "InjectmeLeave"},
+    -- opts = { mode = "all"  },
+  },
+
 }
