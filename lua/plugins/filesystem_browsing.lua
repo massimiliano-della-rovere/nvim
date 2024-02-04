1585975d4809affd348f4de4f4d5f429e8f9c164
Massimiliano <massimiliano.dellarovere@gmail.com>
2024-02-04 22:26:10 +0100

    added git integration for oil

--- lua/plugins/filesystem_browsing.lua
+++ lua/plugins/filesystem_browsing.lua
@@ -85,4 +85,16 @@ return {
     end
   },
 
+  -- git integration for oil
+  {
+    -- https://github.com/refractalize/oil-git-status.nvim
+    "refractalize/oil-git-status.nvim",
+
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+
+    config = true,
+  },
+
 }
