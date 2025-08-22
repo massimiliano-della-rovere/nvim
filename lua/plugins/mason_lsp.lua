8c115a20a08e39b038b4e23ecfc63cbd4d7f6c74
Massimiliano <massimiliano.dellarovere@gmail.com>
2025-08-22 13:35:04 +0200

    shellcheck non è un lsp

--- lua/plugins/mason_lsp.lua
+++ lua/plugins/mason_lsp.lua
@@ -7,7 +7,6 @@ local default_language_servers = {
   "jsonls",                -- JSON
   "lua_ls",                -- LUA
   "pyright",               -- Python
-  "shellcheck",            -- BASh
   "sqlls",                 -- SQL
   "taplo",                 -- TOML
   "ts_ls",                 -- Javascript and Typescript
