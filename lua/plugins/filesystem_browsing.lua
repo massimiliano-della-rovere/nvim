c3d13a3bab2c2b789778b70f2215e91e7d28f51a
Massimiliano della Rovere <massimiliano.dellarovere@dedalus.eu>
2025-10-06 09:32:54 +0200

    fix lsp file operations

--- lua/plugins/filesystem_browsing.lua
+++ lua/plugins/filesystem_browsing.lua
@@ -34,13 +34,46 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim",
+      "3rd/image.nvim",
     },
     config = function()
       vim.keymap.set(
         "n", "<leader>f ",
         "<CMD>Neotree filesystem reveal left<CR>",
         { desc = "Neotree: Open filebrowser" })
+    end,
+  },
+
+  -- LSP filesystem operations
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    -- Uncomment whichever supported plugin(s) you use
+    -- "nvim-tree/nvim-tree.lua",
+    "nvim-neo-tree/neo-tree.nvim",
+    -- "simonmclean/triptych.nvim"
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+
+      -- this goes into mason_lsp.lua#nvim-lspconfig
+      -- local lspconfig = require('lspconfig')
+      --
+      -- -- Set global defaults for all servers
+      -- lspconfig.util.default_config = vim.tbl_extend(
+      --   'force',
+      --   lspconfig.util.default_config,
+      --   {
+      --     capabilities = vim.tbl_deep_extend(
+      --       "force",
+      --       vim.lsp.protocol.make_client_capabilities(),
+      --       -- returns configured operations if setup() was already called
+      --       -- or default operations if not
+      --       require('lsp-file-operations').default_capabilities(),
+      --     )
+      --   }
+      -- )
     end,
   },
 
--- lua/plugins/mason_lsp.lua
+++ lua/plugins/mason_lsp.lua
@@ -38,6 +38,23 @@ return {
     },
     config = function()
 
+      local lspconfig = require('lspconfig')
+
+      -- Set global defaults for all servers
+      lspconfig.util.default_config = vim.tbl_extend(
+        'force',
+        lspconfig.util.default_config,
+        {
+          capabilities = vim.tbl_deep_extend(
+            "force",
+            vim.lsp.protocol.make_client_capabilities(),
+            -- returns configured operations if setup() was already called
+            -- or default operations if not
+            require('lsp-file-operations').default_capabilities()
+          )
+        }
+      )
+
       -- add border to the :Lsp*-command window
       require("lspconfig.ui.windows").default_options.border = "single"
 
